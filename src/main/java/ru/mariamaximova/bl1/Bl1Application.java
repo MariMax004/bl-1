32c7e2e4d5e0c5ee10423a7ddd23157ae4088eb8
mariamaksimova <Maria.ms004@mail.ru>
2022-02-08 20:38:12 +0300

    first commit

---
+++ src/main/java/ru/mariamaximova/bl1/Bl1Application.java
@@ -0,0 +1,13 @@
+package ru.mariamaximova.bl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bl1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bl1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mariamaximova/bl1/Bl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mariamaximova.bl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bl1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
